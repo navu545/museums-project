c87c2ce69bc14c687f34201fcc8bdd042be867bc
navu545 <navu545@gmail.com>
2024-04-08 12:17:34 +0530

    new file:   src/components/Bank/Bank.css 	new file:   src/components/Bank/Bank.js 	deleted:    src/components/Bank/header1.css 	deleted:    src/components/Bank/header1.js 	modified:   src/components/MuseumsListHolder/MuseumsListHolder.js 	modified:   src/global.css 	modified:   src/pages/BankPage/bank-information.css 	modified:   src/pages/BankPage/bank-information.js

--- src/pages/BankPage/bank-information.js
+++ src/pages/BankPage/bank-information.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
-import Header1 from "../../components/Bank/header1";
+import Bank from "../../components/Bank/Bank";
 import Footer from "../../components/Footer/Footer";
 import "./bank-information.css";
 const BankInformation = () => {
   return (
     <div className="bank-information-container">
       <Navbar />
-      <Header1 />
+      <Bank />
       <Footer />
     </div>
   );
