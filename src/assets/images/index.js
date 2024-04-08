d6061f3a4143dbd04c4808708d21d47c557f919f
navu545 <navu545@gmail.com>
2024-04-08 23:40:58 +0530

    Assets added Changes to be committed: 	modified:   .gitignore 	new file:   src/assets/fonts/Montserrat/Montserrat-Regular.ttf 	new file:   src/assets/fonts/NeuaMachina/Mariupol-Regular.otf 	new file:   src/assets/fonts/NeuaMachina/NeueMachina-Light.otf 	new file:   src/assets/fonts/NeuaMachina/NeueMachina-Regular.otf 	new file:   src/assets/fonts/NeuaMachina/NeueMachina-Ultrabold.otf 	new file:   src/assets/fonts/OpenSans/OpenSans-Bold.ttf 	new file:   src/assets/fonts/OpenSans/OpenSans-SemiBold.ttf 	new file:   src/assets/images/Arrow-Select-left.png 	new file:   src/assets/images/Arrow-Select-right.png 	new file:   src/assets/images/Back.png 	new file:   src/assets/images/BankClover.png 	new file:   src/assets/images/CloverQR.png 	new file:   src/assets/images/FooterShadowProject.svg 	new file:   src/assets/images/HeaderImageStraight.png 	new file:   src/assets/images/HeaderImageTilt.png 	new file:   src/assets/images/Image2x.png 	new file:   src/assets/images/Microphone.png 	new file:   src/assets/images/MuseumB&W.png 	new file:   src/assets/images/NavbarShadowProject.svg 	new file:   src/assets/images/PayPalTwo.png 	new file:   src/assets/images/Paypal.png 	new file:   src/assets/images/PaypalQR.png 	new file:   src/assets/images/SaveUkranianArt.png 	new file:   src/assets/images/Slider1.png 	new file:   src/assets/images/Slider10.png 	new file:   src/assets/images/Slider11.png 	new file:   src/assets/images/Slider2.png 	new file:   src/assets/images/Slider3.png 	new file:   src/assets/images/Slider4.png 	new file:   src/assets/images/TestimonyCard1.png 	new file:   src/assets/images/TestimonyCard2.png 	new file:   src/assets/images/TestimonyDetailLarge.png 	new file:   src/assets/images/ThankYou.png 	new file:   src/assets/images/Twitter.svg 	new file:   src/assets/images/YellowTape1.png 	new file:   src/assets/images/YellowTape2.png 	new file:   src/assets/images/aboutusnew.png 	new file:   src/assets/images/arrow.png 	new file:   src/assets/images/coloredmuseum.png 	new file:   src/assets/images/howitworks1.svg 	new file:   src/assets/images/howitworks2.svg 	new file:   src/assets/images/howitworks3.svg 	new file:   src/assets/images/index.js 	new file:   src/assets/images/instagram.svg 	new file:   src/assets/images/leftbutton.png 	new file:   src/assets/images/listSuppliesOpen.svg 	new file:   src/assets/images/moreimages.svg 	new file:   src/assets/images/museumexam.png 	new file:   src/assets/images/museumexam2.png 	new file:   src/assets/images/museumexam3.png 	new file:   src/assets/images/museumexam4.png 	new file:   src/assets/images/previousImage.png 	new file:   src/assets/images/rightbutton.png 	new file:   src/assets/images/rightbutton.svg 	new file:   src/assets/images/slider5.png 	new file:   src/assets/images/slider6.png 	new file:   src/assets/images/slider7.png 	new file:   src/assets/images/slider8.png 	new file:   src/assets/images/slider9.png

---
+++ src/assets/images/index.js
@@ -0,0 +1,99 @@
+import YellowTape1 from "./YellowTape1.png";
+import YellowTape2 from "./YellowTape2.png";
+import Microphone from "./Microphone.png";
+import HeaderImageStraight from "./HeaderImageStraight.png";
+import NavbarShadowProject from "./NavbarShadowProject.svg";
+import FooterShadowProject from "./FooterShadowProject.svg";
+import SaveUkranianArt from "./SaveUkranianArt.png";
+import MuseumBW from "./MuseumB&W.png";
+import instagram from "./instagram.svg";
+import Slider10 from "./Slider10.png";
+import Slider11 from "./Slider11.png";
+import HeaderImageTilt from "./HeaderImageTilt.png";
+import Slider1 from "./Slider1.png";
+import Slider2 from "./Slider2.png";
+import Slider3 from "./Slider3.png";
+import Slider4 from "./Slider4.png";
+import Slider5 from "./slider5.png";
+import Slider6 from "./slider6.png";
+import Slider7 from "./slider7.png";
+import Slider8 from "./slider8.png";
+import Slider9 from "./slider9.png";
+import Twitter from "./Twitter.svg";
+import CloverQR from "./CloverQR.png";
+import PayPalQR from "./PaypalQR.png";
+import BankClover from './BankClover.png';
+import PayPal from "./Paypal.png";
+import TestimonyCard2 from "./TestimonyCard2.png";
+import TestimonyCard1 from "./TestimonyCard1.png";
+import moreimages from "./moreimages.svg";
+import museumexam from "./museumexam.png";
+import museumexam2 from "./museumexam2.png";
+import museumexam3 from "./museumexam3.png";
+import museumexam4 from "./museumexam4.png";
+import rightbutton from "./rightbutton.png";
+import leftbutton from "./leftbutton.png";
+import back from "./Back.png";
+import TestimonyDetailLarge from "./TestimonyDetailLarge.png";
+import howitworks1 from "./howitworks1.svg";
+import howitworks2 from "./howitworks2.svg";
+import howitworks3 from "./howitworks3.svg";
+import listSuppliesOpen from "./listSuppliesOpen.svg";
+import aboutusnew from "./aboutusnew.png";
+import arrowselectleft from "./Arrow-Select-left.png";
+import arrowselectright from "./Arrow-Select-right.png";
+import museumcolored from "./coloredmuseum.png";
+import thankYou from "./ThankYou.png";
+import previousImage from "./previousImage.png";
+import paypalTwo from "./PayPalTwo.png";
+
+export {
+  paypalTwo,
+  previousImage,
+  thankYou,
+  museumcolored,
+  arrowselectright,
+  arrowselectleft,
+  aboutusnew,
+  listSuppliesOpen,
+  howitworks1,
+  howitworks2,
+  howitworks3,
+  TestimonyDetailLarge,
+  back,
+  leftbutton,
+  rightbutton,
+  museumexam4,
+  museumexam3,
+  museumexam2,
+  museumexam,
+  moreimages,
+  TestimonyCard1,
+  TestimonyCard2,
+  CloverQR,
+  PayPalQR,
+  BankClover,
+  PayPal,
+  SaveUkranianArt,
+  MuseumBW,
+  YellowTape2,
+  YellowTape1,
+  Microphone,
+  HeaderImageStraight,
+  instagram,
+  Slider10,
+  Slider11,
+  HeaderImageTilt,
+  Slider1,
+  Slider2,
+  Slider3,
+  Slider4,
+  Slider5,
+  Slider6,
+  Slider7,
+  Slider8,
+  Slider9,
+  Twitter,
+  NavbarShadowProject,
+  FooterShadowProject,
+};
