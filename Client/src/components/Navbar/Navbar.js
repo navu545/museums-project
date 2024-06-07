7348c03d4b009cfb70cfa6e3130ed6892ce040bc
navu545 <navu545@gmail.com>
2024-06-07 13:40:49 +0530

    routing fix

--- Client/src/components/Navbar/Navbar.js
+++ Client/src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import {React,useState} from "react";
-import { logonew } from "../../assets/images";
+// import { logonew } from "../../assets/images";
 import "./Navbar.css";
 import { Link } from 'react-router-dom';
 
@@ -16,7 +16,10 @@ const Navbar = ({toggleScroll,scrollTo}) => {
           <div className="navbar-logo-container">
             <div className="navbar-logo">
               <Link to="/">
-                <img src={logonew} alt="navbar-logo"></img>
+                <img
+                  src="https://ik.imagekit.io/shadows/Shadows-project/LogoNew.svg"
+                  alt="navbar-logo"
+                ></img>
               </Link>
             </div>
           </div>
