17f828d068dc97dce34be045583a108b3b17126a
navu545 <navu545@gmail.com>
2024-06-08 11:35:29 +0530

    testing changes for further optimization

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
-import App from "./App"; // Assuming App.js contains your root component
+import App from "./App"; // App.js contains your root component
 import "./global.css";
 
 
